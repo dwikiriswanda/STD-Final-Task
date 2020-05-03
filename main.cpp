8f3edbd4cf693523ed3956a41cf2297857f4a5cb
dwikiriswanda <m.dwikiriswanda@gmail.com>
2020-05-03 21:26:47 +0700

    Update some function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
-#include <iostream>
+#include "menu.h"
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+int main() {
+
+  listMahasiswa LP;
+  listMataKuliah LC;
+  listRelasi LR;
+  addressMataKuliah C;
+  addressMahasiswa P;
+  addressRelasi R;
+
+  createListMahasiswa(LP);
+  createListMataKuliah(LC);
+  createListRelasi(LR);
+
+  menu();
+
+  return 0;
 }
